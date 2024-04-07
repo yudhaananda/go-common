2bd7dfeb1674a8bc77279439e420b16a9f4349d8
Yudha Ananda Kresna <yakresna@pvg.co.id>
2024-04-08 00:58:49 +0700

    feat: add json response

---
+++ response/response.go
@@ -0,0 +1,27 @@
+package response
+
+type Response struct {
+	Meta   Meta        `json:"meta"`
+	Data   interface{} `json:"data"`
+	Errors interface{} `json:"errors"`
+}
+
+type Meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+}
+
+func APIResponse(message string, code int, status string, data interface{}, errors interface{}) Response {
+	meta := Meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+	jsonResponse := Response{
+		Meta:   meta,
+		Data:   data,
+		Errors: errors,
+	}
+	return jsonResponse
+}
